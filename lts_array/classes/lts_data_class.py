b21e19c0e31882efe89fdf72b1197ba32651d7c7
Liam Toney <ltoney@usgs.gov>
2025-05-09 15:17:18 -0600

    Add `rij` arg for `build_data_arrays()`

--- lts_array/classes/lts_data_class.py
+++ lts_array/classes/lts_data_class.py
@@ -17,13 +17,20 @@ class DataBin:
         self.window_overlap = window_overlap
         self.alpha = alpha
 
-    def build_data_arrays(self, st, latlist, lonlist, remove_elements=None):
+    def build_data_arrays(self, st, latlist, lonlist, rij=None, remove_elements=None):
         """ Collect basic data from stream file. Project lat/lon into r_ij coordinates.
+
+        Alternatively, if ``rij`` is provided, ``latlist`` and ``lonlist`` are not used
+        and the provided Cartesion coordinates ``rij`` are used instead.
 
         Args:
             st (stream): An obspy stream object.
             latlist (list): A list of latitude points.
             lonlist (list): A list of longitude points.
+            rij (array or None): A NumPy array with the first row corresponding to
+                cartesian "X" - coordinates and the second row corresponding to
+                cartesian "Y" - coordinates, in units of km. If this is not ``None``
+                then ``latlist`` and ``lonlist`` are ignored.
             remove_elements (list): A list of elements to remove before processing. Python numbering is used, so "[0]" removes the first element.
         """
         # Check that all traces have the same length
@@ -35,8 +42,16 @@ class DataBin:
             remove_elements = np.sort(remove_elements)
             for jj in range(0, len(remove_elements)):
                 st.remove(st[remove_elements[jj]])
-                latlist.remove(latlist[remove_elements[jj]])
-                lonlist.remove(lonlist[remove_elements[jj]])
+                if rij is None:
+                    latlist.remove(latlist[remove_elements[jj]])
+                    lonlist.remove(lonlist[remove_elements[jj]])
+                else:
+                    _x, _y = rij
+                    _x_list = list(_x)
+                    _y_list = list(_y)
+                    _x_list.remove(_x_list[remove_elements[jj]])
+                    _y_list.remove(_y_list[remove_elements[jj]])
+                    rij = np.array([_x_list, _y_list])
                 remove_elements -= 1
 
         # Save the station name
@@ -69,7 +84,10 @@ class DataBin:
         for ii, tr in enumerate(st):
             self.data[:, ii] = tr.data
         # Set the array coordinates
-        self.rij = self.getrij(latlist, lonlist)
+        if rij is None:
+            self.rij = self.getrij(latlist, lonlist)
+        else:
+            self.rij = rij
         # Make sure the least squares problem is well-posed
         # rij must have at least 3 elements
         if np.shape(self.rij)[1] < 3:
@@ -92,10 +110,9 @@ class DataBin:
 
         Returns:
             (array):
-            ``rij``: A numpy array with the first row corresponding to
+            ``rij``: A NumPy array with the first row corresponding to
             cartesian "X" - coordinates and the second row
-            corresponding to cartesian "Y" - coordinates.
-
+            corresponding to cartesian "Y" - coordinates, in units of km.
         """
 
         # Check that the lat-lon arrays are the same size.
